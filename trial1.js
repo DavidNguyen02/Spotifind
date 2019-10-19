1941d1c1d5a0a5dff819c1245e966fa0f853aaf2
David <daviddnguyen1337@gmail.com>
2019-10-19 16:01:53 -0400

    More stuff

--- trial1.js
+++ trial1.js
@@ -6,5 +6,9 @@ function addRow(){
     var table = document.getElementById("table");
     var row = table.insertRow();
     var cell = row.insertCell();
-    cell.innerHTML = document.getElementById("message").value;
+    var cell2 = row.insertCell();
+    cell.innerHTML = ""
+    cell2.innerHTML = document.getElementById("message").value;
+    document.getElementById("message").value = "";
+    cell2.classList.add("mine");
 }
